78967f0ed4635627d7a15d3d6b0ca6a9cda42bb5
Preksha <prekshakhabya23@gmail.com>
2022-04-01 00:41:29 +0530

    1 April

--- client/src/users/student/ProfileStudent.js
+++ client/src/users/student/ProfileStudent.js
@@ -1,16 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import DefaultLayoutStudent from "../../component/DefaultLayoutStudent";
 import { FaUser, FaGraduationCap } from "react-icons/fa";
-import { AiFillLayout } from "react-icons/ai";
-import { Steps, Button, message, Form, Input, Select, DatePicker } from "antd";
+import { IoDocumentText } from "react-icons/io5";
+import { Steps, Button, message, Form, Input, Select, Row, Col } from "antd";
 import { useDispatch } from "react-redux";
 import { updateUser } from "../../redux/action/usersAction";
 
 const { Step } = Steps;
 const { Option } = Select;
-
 const user = JSON.parse(localStorage.getItem("user"));
-
 function ProfileStudent() {
   const [current, setCurrent] = React.useState(0);
 
@@ -35,7 +33,7 @@ function ProfileStudent() {
     {
       title: "Resume",
       content: resume(),
-      icon: <AiFillLayout />,
+      icon: <IoDocumentText />,
     },
   ];
   const [personalInfo, setPersonalInfo] = useState();
@@ -43,11 +41,11 @@ function ProfileStudent() {
 
   function onPersonInfoSubmit(values) {
     setPersonalInfo(values);
+    dispatch(updateUser(values));
   }
 
   function onFinalFinish(values) {
     const finalObj = { ...personalInfo, ...values };
-    console.log(finalObj);
     dispatch(updateUser(finalObj));
   }
 
@@ -57,150 +55,185 @@ function ProfileStudent() {
       <Form
         layout="vertical"
         onFinish={onPersonInfoSubmit}
-        // initialValues={user}
+        initialValues={user}
       >
-        <Form.Item
-          label="First Name"
-          name="firstName"
-          rules={[{ required: true, message: "First Name is required" }]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="Last Name"
-          name="lastName"
-          rules={[{ required: true, message: "Last Name is required" }]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item label="Profile Photo" name="profilePhoto"></Form.Item>
-
-        <Form.Item
-          label="Date of Birth"
-          name="dob"
-          rules={[{ required: true, message: "Date of Birth is required" }]}
-        >
-          <DatePicker />
-        </Form.Item>
-        <Form.Item
-          label="Gender"
-          name="gender"
-          rules={[{ required: true, message: "Gender is required" }]}
-        >
-          <Select placeholder="Select a Gender">
-            <Option value="F">Female</Option>
-            <Option value="M">Male</Option>
-            <Option value="O">Other</Option>
-          </Select>
-        </Form.Item>
-        <Form.Item
-          label="College Id"
-          name="collegeid"
-          rules={[{ required: true, message: "College Id is required" }]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="College Email"
-          name="collegeMail"
-          rules={[
-            { required: true, message: "College Email is required" },
-            {
-              type: "email",
-              message: "Please enter valid Email!",
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="Address"
-          name="address"
-          rules={[{ required: true, message: "Address is required" }]}
-        >
-          <Input.TextArea />
-        </Form.Item>
-        <Form.Item
-          label="About"
-          name="about"
-          rules={[{ required: true, message: "About is required" }]}
-        >
-          <Input.TextArea />
-        </Form.Item>
-
-        <Button htmlType="submit">Submit</Button>
+        <Row gutter={16}>
+          <Col lg={8} sm={24}>
+            <Form.Item
+              label="First Name"
+              name="firstName"
+              rules={[{ required: true, message: "First Name is required" }]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col lg={8} sm={24}>
+            <Form.Item
+              label="Last Name"
+              name="lastName"
+              rules={[{ required: true, message: "Last Name is required" }]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col lg={8} sm={24}>
+            <Form.Item
+              label="Date of Birth"
+              name="dob"
+              rules={[{ required: true, message: "Date of Birth is required" }]}
+            >
+              <Input placeholder="dd/mm/yyyy" />
+            </Form.Item>
+          </Col>
+          <Col lg={8} sm={24}>
+            <Form.Item
+              label="Gender"
+              name="gender"
+              rules={[{ required: true, message: "Gender is required" }]}
+            >
+              <Select placeholder="Select a Gender">
+                <Option value="F">Female</Option>
+                <Option value="M">Male</Option>
+                <Option value="O">Other</Option>
+              </Select>
+            </Form.Item>
+          </Col>
+          <Col lg={8} sm={24}>
+            <Form.Item
+              label="College Email"
+              name="collegeMail"
+              rules={[
+                { required: true, message: "College Email is required" },
+                {
+                  type: "email",
+                  message: "Please enter valid Email!",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col lg={8} sm={24}>
+            <Form.Item
+              label="College Id"
+              name="collegeid"
+              rules={[{ required: true, message: "College Id is required" }]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col lg={8} sm={24}>
+            <Form.Item label="Profile Photo" name="profilePhoto"></Form.Item>
+          </Col>
+          <Col lg={24} sm={24}>
+            <Form.Item
+              label="About"
+              required
+              rules={[{ required: true }]}
+              name="about"
+            >
+              <Input.TextArea />
+            </Form.Item>
+          </Col>
+          <Col lg={24} sm={24}>
+            <Form.Item
+              label="Address"
+              required
+              rules={[{ required: true }]}
+              name="address"
+            >
+              <Input.TextArea />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Button htmlType="submit">Update</Button>
       </Form>
     );
   }
 
   function educationDetails() {
     return (
-      <Form
-        layout="vertical"
-        onFinish={onFinalFinish}
-        //  initialValues={user}
-      >
-        <Form.Item
-          label="Institue"
-          name="institute"
-          rules={[{ required: true, message: "Institution Name is required" }]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="Course"
-          name="course"
-          rules={[{ required: true, message: "Course is required" }]}
-        >
-          <Select placeholder="Select a Course">
-            <Option value="B.tech">B. Tech</Option>
-            <Option value="M.Tech">M. Tech</Option>
-          </Select>
-        </Form.Item>
-        <Form.Item
-          label="Branch"
-          name="branch"
-          rules={[{ required: true, message: "Branch is required" }]}
-        >
-          <Select placeholder="Select a Branch">
-            <Option value="CSE">Computer Science Engineering</Option>
-            <Option value="IT">Information Technology</Option>
-            <Option value="EEE">Electrical and Electronics Engineering</Option>
-            <Option value="EI">
-              Electronics and Instrumentation Engineering
-            </Option>
-            <Option value="ECE">
-              Electronics and Communication Engineering
-            </Option>
-            <Option value="MT">Mechatronics Engineering</Option>
-            <Option value="BT">Biotechnology Engineering</Option>
-            <Option value="CE">Chemical Engineering</Option>
-          </Select>
-        </Form.Item>
-        <Form.Item
-          label="Starting Year"
-          name="yos"
-          rules={[{ required: true, message: "Starting Year is required" }]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="Passing Year (or expected)"
-          name="yop"
-          rules={[{ required: true, message: "Passing Year is required" }]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          label="CGPA"
-          name="cgpa"
-          rules={[{ required: true, message: "CGPA is required" }]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Button htmlType="submit">Submit</Button>
+      <Form initialValues={user} layout="vertical" onFinish={onFinalFinish}>
+        <Row gutter={16}>
+          <Col lg={8} sm={24}>
+            <Form.Item
+              label="Institue"
+              name={["education", "institute"]}
+              rules={[
+                {
+                  required: true,
+                  message: "Institution Name is required",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col lg={8} sm={24}>
+            <Form.Item
+              label="Course"
+              name={["education", "course"]}
+              rules={[{ required: true, message: "Course is required" }]}
+            >
+              <Select placeholder="Select a Course">
+                <Option value="B.tech">B. Tech</Option>
+                <Option value="M.Tech">M. Tech</Option>
+              </Select>
+            </Form.Item>
+          </Col>
+          <Col lg={8} sm={24}>
+            <Form.Item
+              label="Branch"
+              name={["education", "branch"]}
+              rules={[{ required: true, message: "Branch is required" }]}
+            >
+              <Select placeholder="Select a Branch">
+                <Option value="CSE">Computer Science Engineering</Option>
+                <Option value="IT">Information Technology</Option>
+                <Option value="EEE">
+                  Electrical and Electronics Engineering
+                </Option>
+                <Option value="EI">
+                  Electronics and Instrumentation Engineering
+                </Option>
+                <Option value="ECE">
+                  Electronics and Communication Engineering
+                </Option>
+                <Option value="MT">Mechatronics Engineering</Option>
+                <Option value="BT">Biotechnology Engineering</Option>
+                <Option value="CE">Chemical Engineering</Option>
+              </Select>
+            </Form.Item>
+          </Col>
+          <Col lg={8} sm={24}>
+            <Form.Item
+              label="Starting Year"
+              name={["education", "yos"]}
+              rules={[{ required: true, message: "Starting Year is required" }]}
+            >
+              <Input placeholder="yyyy" />
+            </Form.Item>
+          </Col>
+          <Col lg={8} sm={24}>
+            <Form.Item
+              label="Passing Year (expected)"
+              name={["education", "yop"]}
+              rules={[{ required: true, message: "Passing Year is required" }]}
+            >
+              <Input placeholder="yyyy" />
+            </Form.Item>
+          </Col>
+          <Col lg={8} sm={24}>
+            <Form.Item
+              label="CGPA"
+              name={["education", "cgpa"]}
+              rules={[{ required: true, message: "CGPA is required" }]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Button htmlType="submit">Update</Button>
       </Form>
     );
   }
