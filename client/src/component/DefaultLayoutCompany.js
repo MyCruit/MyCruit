5743f33831eea8687ec392e353c4735ba3e16ab9
Preksha <prekshakhabya23@gmail.com>
2022-04-04 00:41:01 +0530

    Navbar

--- client/src/component/DefaultLayoutCompany.js
+++ client/src/component/DefaultLayoutCompany.js
@@ -11,6 +11,8 @@ import {
 import { Link } from "react-router-dom";
 
 const { Header, Sider, Content } = Layout;
+const user = JSON.parse(localStorage.getItem("user"));
+
 const onConfirm = (e) => {
   window.location = "/login";
   window.localStorage.clear();
@@ -20,55 +22,77 @@ const onConfirm = (e) => {
 class DefaultLayoutCompany extends React.Component {
   render() {
     return (
-      <Layout className="layout">
-        <Sider
-          breakpoint="lg"
-          collapsedWidth="0"
+      <Layout>
+        <Header
           style={{
-            position: "sticky",
-            overflow: "auto",
-            height: "100%",
-            top: 0,
+            padding: 0,
+            zIndex: 9999,
           }}
         >
-          <div className="logo">
-            <img src={pic} alt="Logo"></img>
-            <h1>MyCruit</h1>
+          <div className="flex justify-content-between">
+            <div className="logo">
+              <img src={pic} alt="Logo"></img>
+              <h1>MyCruit</h1>
+            </div>
+            <div>
+              <h5 className="mr-5 mb-4">{user.username}</h5>
+            </div>
           </div>
-          <Menu defaultSelectedKeys={[window.location.pathname]}>
-            <Menu.Item key="/" icon={<HomeOutlined />}>
-              <Link to="/">Home</Link>
-            </Menu.Item>
-            <Menu.Item key="/profile" icon={<UserOutlined />}>
-              <Link to="/profile">Profile</Link>
-            </Menu.Item>
-            <Menu.Item key="/post" icon={<UploadOutlined />}>
-              <Link to="/post">Post</Link>
-            </Menu.Item>
-            <Menu.Item key="/logout" icon={<LogoutOutlined />}>
-              <Popconfirm
-                title="Are you sure you want to quit?"
-                placement="right"
-                onConfirm={onConfirm}
-                okText="Quit"
-                cancelText="Cancel"
-              >
-                <Link to="">Logout</Link>
-              </Popconfirm>
-            </Menu.Item>
-          </Menu>
-        </Sider>
+        </Header>
 
         <Layout>
-          <Header
+          <Sider
+            breakpoint="lg"
+            collapsedWidth="0"
             style={{
-              padding: 0,
               position: "sticky",
-              overflow: "auto",
+              height: "100%",
               top: 0,
-              zIndex: 9999,
             }}
-          ></Header>
+          >
+            <Menu defaultSelectedKeys={[window.location.pathname]}>
+              <Menu.Item key="/">
+                <Link to="/">
+                  <div className="navbar">
+                    <HomeOutlined className="navbar-icon m-1" />
+                    <h6>Home</h6>
+                  </div>
+                </Link>
+              </Menu.Item>
+              <Menu.Item key="/profile">
+                <Link to="/profile">
+                  <div className="navbar">
+                    <UserOutlined className="navbar-icon m-1" />
+                    <h6>Profile</h6>
+                  </div>
+                </Link>
+              </Menu.Item>
+              <Menu.Item key="/post">
+                <Link to="/post">
+                  <div className="navbar">
+                    <UploadOutlined className="navbar-icon m-1" />
+                    <h6>Post</h6>
+                  </div>
+                </Link>
+              </Menu.Item>
+              <Menu.Item key="/logout">
+                <Popconfirm
+                  title="Are you sure you want to quit?"
+                  placement="right"
+                  onConfirm={onConfirm}
+                  okText="Quit"
+                  cancelText="Cancel"
+                >
+                  <Link to="">
+                    <div className="navbar">
+                      <LogoutOutlined className="navbar-icon m-1" />
+                      <h6>Logout</h6>
+                    </div>
+                  </Link>
+                </Popconfirm>
+              </Menu.Item>
+            </Menu>
+          </Sider>
           <Content>{this.props.children}</Content>
         </Layout>
       </Layout>
--- client/src/component/DefaultLayoutStudent.js
+++ client/src/component/DefaultLayoutStudent.js
@@ -12,6 +12,7 @@ import {
 
 import { Link } from "react-router-dom";
 const { Header, Sider, Content } = Layout;
+const user = JSON.parse(localStorage.getItem("user"));
 
 const onConfirm = (e) => {
   window.location = "/login";
@@ -22,55 +23,82 @@ class DefaultLayoutStudent extends React.Component {
   render() {
     return (
       <Layout>
-        <Sider
-          breakpoint="lg"
-          collapsedWidth="0"
+        <Header
           style={{
-            position: "sticky",
-            height: "100%",
-            top: 0,
+            padding: 0,
+            zIndex: 9998,
           }}
         >
-          <div className="logo">
-            <img src={pic} alt="Logo"></img>
-            <h1>MyCruit</h1>
+          <div className="flex justify-content-between">
+            <div className="logo">
+              <img src={pic} alt="Logo"></img>
+              <h1>MyCruit</h1>
+            </div>
+            <div>
+              <h5 className="mr-5 mb-3">{user.username}</h5>
+            </div>
           </div>
-          <Menu defaultSelectedKeys={[window.location.pathname]}>
-            <Menu.Item key="/" icon={<HomeOutlined />}>
-              <Link to="/">Home</Link>
-            </Menu.Item>
-            <Menu.Item key="/profile" icon={<UserOutlined />}>
-              <Link to="/profile">Profile</Link>
-            </Menu.Item>
-            <Menu.Item key="/resume" icon={<LayoutOutlined />}>
-              <Link to="/resume">Resume</Link>
-            </Menu.Item>
-            <Menu.Item key="/appliedJobs" icon={<CarryOutOutlined />}>
-              <Link to="/appliedJobs">Applied Jobs</Link>
-            </Menu.Item>
-            <Menu.Item key="/logout" icon={<LogoutOutlined />}>
-              <Popconfirm
-                title="Are you sure you want to quit?"
-                placement="right"
-                onConfirm={onConfirm}
-                okText="Quit"
-                cancelText="Cancel"
-              >
-                <Link to="">Logout</Link>
-              </Popconfirm>
-            </Menu.Item>
-          </Menu>
-        </Sider>
+        </Header>
 
         <Layout>
-          <Header
+          <Sider
             style={{
-              padding: 0,
               position: "sticky",
+              height: "100%",
               top: 0,
-              zIndex: 9998,
             }}
-          ></Header>
+          >
+            <Menu defaultSelectedKeys={[window.location.pathname]}>
+              <Menu.Item key="/">
+                <Link to="/">
+                  <div className="navbar">
+                    <HomeOutlined className="navbar-icon m-1" />
+                    <h6>Home</h6>
+                  </div>
+                </Link>
+              </Menu.Item>
+              <Menu.Item key="/profile">
+                <Link to="/profile">
+                  <div className="navbar">
+                    <UserOutlined className=" navbar-icon m-1" />
+                    <h6>Profile</h6>
+                  </div>
+                </Link>
+              </Menu.Item>
+              <Menu.Item key="/resume">
+                <Link to="/resume">
+                  <div className="navbar">
+                    <LayoutOutlined className="navbar-icon m-1" />
+                    <h6>Resume</h6>
+                  </div>
+                </Link>
+              </Menu.Item>
+              <Menu.Item key="/appliedJobs">
+                <Link to="/appliedJobs">
+                  <div className="navbar">
+                    <CarryOutOutlined className="navbar-icon m-1" />
+                    <h6>Applied</h6>
+                  </div>
+                </Link>
+              </Menu.Item>
+              <Menu.Item key="/logout">
+                <Popconfirm
+                  title="Are you sure you want to quit?"
+                  placement="right"
+                  onConfirm={onConfirm}
+                  okText="Quit"
+                  cancelText="Cancel"
+                >
+                  <Link to="">
+                    <div className="navbar">
+                      <LogoutOutlined className="navbar-icon m-1" />
+                      <h6>Logout</h6>
+                    </div>
+                  </Link>
+                </Popconfirm>
+              </Menu.Item>
+            </Menu>
+          </Sider>
           <Content>{this.props.children}</Content>
         </Layout>
       </Layout>
